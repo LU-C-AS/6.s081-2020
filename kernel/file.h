b73f2bda0322e2aa7c2ffe093f02ae5379f75557
lucas <z17328173833@126.com>
2023-09-13 16:41:10 +0800

    s

--- kernel/file.h
+++ kernel/file.h
@@ -1,3 +1,7 @@
+#pragma once
+#include "fs.h"
+#include "sleeplock.h"
+#include "types.h"
 struct file {
 #ifdef LAB_NET
   enum { FD_NONE, FD_PIPE, FD_INODE, FD_DEVICE, FD_SOCK } type;
@@ -12,30 +16,30 @@ struct file {
 #ifdef LAB_NET
   struct sock *sock; // FD_SOCK
 #endif
-  uint off;          // FD_INODE
-  short major;       // FD_DEVICE
+  uint off;    // FD_INODE
+  short major; // FD_DEVICE
 };
 
-#define major(dev)  ((dev) >> 16 & 0xFFFF)
-#define minor(dev)  ((dev) & 0xFFFF)
-#define	mkdev(m,n)  ((uint)((m)<<16| (n)))
+#define major(dev) ((dev) >> 16 & 0xFFFF)
+#define minor(dev) ((dev)&0xFFFF)
+#define mkdev(m, n) ((uint)((m) << 16 | (n)))
 
 // in-memory copy of an inode
 struct inode {
-  uint dev;           // Device number
-  uint inum;          // Inode number
-  int ref;            // Reference count
+  uint dev;              // Device number
+  uint inum;             // Inode number
+  int ref;               // Reference count
   struct sleeplock lock; // protects everything below here
-  int valid;          // inode has been read from disk?
+  int valid;             // inode has been read from disk?
 
-  short type;         // copy of disk inode
+  short type; // copy of disk inode
   short major;
   short minor;
   short nlink;
   uint size;
 #ifdef SOL_FS
 #else
-  uint addrs[NDIRECT+1];
+  uint addrs[NDIRECT + 1];
 #endif
 };
 
@@ -48,4 +52,4 @@ struct devsw {
 extern struct devsw devsw[];
 
 #define CONSOLE 1
-#define STATS   2
+#define STATS 2
--- kernel/proc.h
+++ kernel/proc.h
@@ -1,3 +1,8 @@
+#pragma once
+#include "param.h"
+#include "riscv.h"
+#include "spinlock.h"
+#include "types.h"
 // Saved registers for kernel context switches.
 struct context {
   uint64 ra;
@@ -20,10 +25,10 @@ struct context {
 
 // Per-CPU state.
 struct cpu {
-  struct proc *proc;          // The process running on this cpu, or null.
-  struct context context;     // swtch() here to enter scheduler().
-  int noff;                   // Depth of push_off() nesting.
-  int intena;                 // Were interrupts enabled before push_off()?
+  struct proc *proc;      // The process running on this cpu, or null.
+  struct context context; // swtch() here to enter scheduler().
+  int noff;               // Depth of push_off() nesting.
+  int intena;             // Were interrupts enabled before push_off()?
 };
 
 extern struct cpu cpus[NCPU];
@@ -87,17 +92,18 @@ struct proc {
   struct spinlock lock;
 
   // p->lock must be held when using these:
-  enum procstate state;        // Process state
-  struct proc *parent;         // Parent process
-  void *chan;                  // If non-zero, sleeping on chan
-  int killed;                  // If non-zero, have been killed
-  int xstate;                  // Exit status to be returned to parent's wait
-  int pid;                     // Process ID
+  enum procstate state; // Process state
+  struct proc *parent;  // Parent process
+  void *chan;           // If non-zero, sleeping on chan
+  int killed;           // If non-zero, have been killed
+  int xstate;           // Exit status to be returned to parent's wait
+  int pid;              // Process ID
 
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
   pagetable_t pagetable;       // User page table
+  pagetable_t kpagetable;      // kernel pagetable
   struct trapframe *trapframe; // data page for trampoline.S
   struct context context;      // swtch() here to run process
   struct file *ofile[NOFILE];  // Open files
